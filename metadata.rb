39acdf84bbbe3cbf96a5a65dc3e62c0c23ed2492
Lonnie VanZandt <lonniev@gmail.com>
2014-12-01 09:02:46 -0700

    add missing depends in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          'Apache 2.0'
 description      'Clones Git Repositories for Users'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends          'ssh_known_hosts'
