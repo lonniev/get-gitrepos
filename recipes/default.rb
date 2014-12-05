aab999a406d4c1717979350b23e65a20f7e82b26
Lonnie VanZandt <lonniev@gmail.com>
2014-12-04 22:05:33 -0700

    tabify and remove logging

--- recipes/default.rb
+++ recipes/default.rb
@@ -32,26 +32,26 @@ node['get-gitrepos']['repos'].each do |repoSpec|
     
     if ( !Pathname.new( userHomePath ).directory? )
         
-        log "message" do
-            message "user #{gitUserName} missing from data_bags/users or not specified to be in devops group."
-            level :warn
-        end
-        
-        next
+      log "message" do
+        message "user #{gitUserName} missing from data_bags/users or not specified to be in devops group."
+        level :warn
+      end
+      
+      next
     end
     
     destPath = Pathname.new( repo['destination'].sub( /~/, "#{homeDir}/" ) )
     
     destPath.descend{ |dir|
     
-        directory dir.to_s do
-            owner gitUserName
-            group gitUserName
-            
-            action :create
-            
-            not_if { dir.exist?() }
-        end
+      directory dir.to_s do
+        owner gitUserName
+        group gitUserName
+        
+        action :create
+        
+        not_if { dir.exist?() }
+      end
     }
 
     keyId = repo[ 'key_id' ]
@@ -74,11 +74,6 @@ Host #{repo['host']}
 EOT
     end
     
-    log "message" do
-      message "content would be #{Dir.glob( sshDir.join( "config_*" ) ).map{ |cfg| IO.read( cfg ) }.join( "\n" ) }"
-      level :warn
-    end
-    
     file sshDir.join( "config" ).to_s do
       owner gitUserName
       group gitUserName
@@ -90,61 +85,61 @@ EOT
     git_key = Chef::EncryptedDataBagItem.load( "private_keys", keyId )
 
     file idFile.to_s do
-        owner gitUserName
-        group gitUserName
-        mode 0600
-        
-        content git_key['private']
+      owner gitUserName
+      group gitUserName
+      mode 0600
+      
+      content git_key['private']
     end
     
     file idFile.sub_ext( ".pub" ).to_s do
-        owner gitUserName
-        group gitUserName
-        mode 0644
-        
-        content git_key['public']
+      owner gitUserName
+      group gitUserName
+      mode 0644
+      
+      content git_key['public']
     end
     
 # in the context of the requested user, clone, checkout, and branch the repo
     execute "as user #{gitUserName}, clone #{repo['url']}" do
-        cwd destPath.to_s
-        user gitUserName
-        group gitUserName
-        
-        command "git clone -n #{repo['url']} #{destPath}"
-        
-        not_if { File.exists?( File.join( destPath, ".git" ) ) }
+      cwd destPath.to_s
+      user gitUserName
+      group gitUserName
+      
+      command "git clone -n #{repo['url']} #{destPath}"
+      
+      not_if { File.exists?( File.join( destPath, ".git" ) ) }
     end
     
     execute "as user #{gitUserName}, checkout #{repo['remote-branch-name'] || 'master'} from #{repo['url']}" do
-        cwd destPath.to_s
-        user gitUserName
-        group gitUserName
-        
-        command "git checkout #{repo['remote-branch-name'] || 'master'}"
+      cwd destPath.to_s
+      user gitUserName
+      group gitUserName
+      
+      command "git checkout #{repo['remote-branch-name'] || 'master'}"
     end
     
     execute "as user #{gitUserName}, switch to branch #{repo['local-branch-name']}" do
-        cwd destPath.to_s
-        user gitUserName
-        group gitUserName
-        
-        command "git checkout #{repo['local-branch-name']}"
-        only_if "git show-branch #{repo['local-branch-name']}", :cwd => destPath.to_s, :user => gitUserName
+      cwd destPath.to_s
+      user gitUserName
+      group gitUserName
+      
+      command "git checkout #{repo['local-branch-name']}"
+      only_if "git show-branch #{repo['local-branch-name']}", :cwd => destPath.to_s, :user => gitUserName
     end
     
     execute "as user #{gitUserName}, create branch of #{repo['url']} as #{repo['local-branch-name']}" do
-        cwd destPath.to_s
-        user gitUserName
-        group gitUserName
-        
-        command "git checkout -b #{repo['local-branch-name']} #{repo['revision'] || 'HEAD'}"
-        not_if "git show-branch #{repo['local-branch-name']}", :cwd => destPath.to_s, :user => gitUserName
+      cwd destPath.to_s
+      user gitUserName
+      group gitUserName
+      
+      command "git checkout -b #{repo['local-branch-name']} #{repo['revision'] || 'HEAD'}"
+      not_if "git show-branch #{repo['local-branch-name']}", :cwd => destPath.to_s, :user => gitUserName
     end
 
     log "message" do
-        message "Cloned #{repoName} for user #{gitUserName} into #{destPath}."
-        level :info
+      message "Cloned #{repoName} for user #{gitUserName} into #{destPath}."
+      level :info
     end
   end
 end
